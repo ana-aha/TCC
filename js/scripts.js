ea2d2a5acd065091e2974bbf0d1fcf7ae3a74848
Beatriz <BEATRIZ.CRUZ84@ETEC.SP.GOV.BR>
2024-10-03 17:04:33 +0000

    save1

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,28 @@
 window.addEventListener('DOMContentLoaded', event => {
 
 
+    //acessibilidade 
+    let fontSize = 16;
+
+    document.getElementById('increaseFont').addEventListener('click', () => {
+        fontSize += 2;
+        document.body.style.fontSize = fontSize + 'px';
+    });
+
+    document.getElementById('decreaseFont').addEventListener('click', () => {
+        if (fontSize > 10) { // Limita o tamanho mínimo
+            fontSize -= 2;
+            document.body.style.fontSize = fontSize + 'px';
+        }
+    });
+
+    document.getElementById('toggleMode').addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        const modeButton = document.getElementById('toggleMode');
+        modeButton.querySelector('img').alt =
+            modeButton.querySelector('img').alt === 'Modo Escuro' ? 'Modo Claro' : 'Modo Escuro';
+    });
+
     //carrosel
     let index = 0;
 
